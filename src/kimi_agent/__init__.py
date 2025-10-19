9918b21c69e775eb4564c7c1443d809b6f474006
jrapisarda <jrapisarda@live.com>
2025-10-18 20:18:36 -0600

    Removing AI Slop

--- src/kimi_agent/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Kimi Coding Agent package."""
-
-from .config import RunConfig, AgentConfig, DEFAULT_MODEL
-from .orchestrator import AgentOrchestrator
-
-__all__ = ["RunConfig", "AgentConfig", "AgentOrchestrator", "DEFAULT_MODEL"]
